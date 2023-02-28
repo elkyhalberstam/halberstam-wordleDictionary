399115243bf3752031eabf0345c2dccf424fea18
unknown <elkyhalberstam@gmail.com>
2023-02-28 16:01:12 -0500

    I delted/removed WOrdleGameFrame and WordleGameMain from branch game in attempt to move it to branch gui"

--- src/main/java/halberstam/wordle/WordleGameMain.java
+++
@@ -1,8 +0,0 @@
-package halberstam.wordle;
-
-public class WordleGameMain {
-    public static void main(String[] args) {
-        WordleGameFrame frame = new WordleGameFrame();
-        frame.setVisible(true);
-    }
-}
