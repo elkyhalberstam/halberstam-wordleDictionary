77d810b48591ea96e44c698f4b7967a6ccdc2f0d
unknown <elkyhalberstam@gmail.com>
2023-02-28 15:26:25 -0500

    adding WOrdleGameFrame adn WordleGameMain

---
+++ src/main/java/halberstam/wordle/WordleGameMain.java
@@ -0,0 +1,8 @@
+package halberstam.wordle;
+
+public class WordleGameMain {
+    public static void main(String[] args) {
+        WordleGameFrame frame = new WordleGameFrame();
+        frame.setVisible(true);
+    }
+}
